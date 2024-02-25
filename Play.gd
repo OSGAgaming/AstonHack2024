134f016fe4675aa2bba07016f75fa9b1754ab3f1
davidovski <david@davidovski.xyz>
2024-02-25 04:26:04 +0000

    move tv

--- Play.gd
+++ Play.gd
@@ -4,6 +4,8 @@ extends Button
 @onready var playCouroutine = 0;
 @onready var centerStage = get_node("/root/Node3D/CenterStage")
 @onready var player = get_tree().get_root().get_node("Node3D/player");
+@onready var eyes = get_tree().get_root().get_node("Node3D/player/Eyes");
+@onready var goose = get_tree().get_root().get_node("Node3D/Goose");
 
 var music = preload("res://assets/gameshow.wav")
 # Called when the node enters the scene tree for the first time.
@@ -30,10 +32,10 @@ func _process(delta):
 			Global.transitionAlpha = (3 -(playCouroutine - 3)) / 3
 			player.position.x = centerStage.position.x;
 			player.position.z = centerStage.position.z;
+			eyes.look_at(goose.position)
 			if playCouroutine >= 6:
 				Global.gameState = Global.GameState.Gameplay;
 				playCouroutineReady = false
-
 
 	pass
 
