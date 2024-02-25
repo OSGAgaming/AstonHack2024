9eef80f8d2cc7599bf269396bb2ece4401e217bd
OSGAgaming <abubakar.tafida@student.manchester.ac.uk>
2024-02-25 04:35:21 +0000

    collect

--- Collectable.gd
+++ Collectable.gd
@@ -1,7 +1,12 @@
 extends MeshInstance3D
 
-const description = "This is an item you can collect";
-const outDescription = "You just collected this item";
+const description = "Looks like an id card";
+const outDescription = "You just picked up an id card with your \n
+Name: woiuhgwergrwef \n 
+Date Of Birth: 05-04-1999"
+
+var within = false;
+var time = 0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,8 +14,23 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Global.currentItem == outDescription:
+		time += delta
+		if time > 3:
+			Global.currentItem = ""
+			queue_free()
+
 	if Global.player != null:
 		var distance = Global.player.transform.origin.distance_to(transform.origin);
-		if distance < 5:
-			Global.currentDescription = description;
+		if distance < 2 and visible:
+			if not within:
+				Global.currentDescription = description;
+			within = true
+			if Input.is_action_just_pressed("Enter"):
+				Global.currentItem = outDescription
+				visible = false
+		else:
+			if within:
+				Global.currentDescription = "";
+			within = false
 			
